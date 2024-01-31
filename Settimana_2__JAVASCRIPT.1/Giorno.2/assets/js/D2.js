f4097245e329ca4e716f11795aef3f3461d5dd4e
vitod-ag <116908757+vitod-ag@users.noreply.github.com>
2024-01-31 11:55:34 +0100

    commit 31/01

--- Settimana_2__JAVASCRIPT.1/Giorno.2/assets/js/D2.js
+++ Settimana_2__JAVASCRIPT.1/Giorno.2/assets/js/D2.js
@@ -250,16 +250,7 @@ console.log(me.skills[2]);
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 let array=[];
-array[0]=1;
-array[1]=2;
-array[2]=3;
-array[3]=4;
-array[4]=5;
-array[5]=6;
-array[6]=7;
-array[7]=8;
-array[8]=9;
-array[9]=10;
+array.push(1,2,3,4,5,6,7,8,9,10);
 console.log(array);
 document.getElementById('array').innerHTML = array;
 
