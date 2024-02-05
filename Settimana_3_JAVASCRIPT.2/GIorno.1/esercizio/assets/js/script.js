81922a6f0470d5826219fcccaa6c3b1a28c96b1a
vitod-ag <116908757+vitod-ag@users.noreply.github.com>
2024-02-05 23:10:44 +0100

    commit

--- Settimana_3_JAVASCRIPT.2/GIorno.1/esercizio/assets/js/script.js
+++ Settimana_3_JAVASCRIPT.2/GIorno.1/esercizio/assets/js/script.js
@@ -79,41 +79,72 @@ addParagraph();
 //  Scrivi una funzione che faccia scomparire la prima lista non ordinata
 // */
 
-// const hideFirstUl = function () { 
-//     const primaLista = document.getElementById('firstList');
-//     primaLista.style.display = 'none';
-// }
+const hideFirstUl = function () { 
+    const primaLista = document.getElementById('firstList');
+    primaLista.style.display='none';        // PER FAR SPARIRE IL DATO
+}
+
+hideFirstUl();
 
 // /* ESERCIZIO 8 
 //  Scrivi una funzione che renda verde il background di ogni lista non ordinata
 // */
 
-// const paintItGreen = function () { 
-//     const liste = document.querySelectorAll('ul');
-//     for (let i=0; i<liste.length; i++){
-//         liste[i].style.backgroundColor = 'green';
-//     }
-// }
+const paintItGreen = function () { 
+    const liste = document.querySelectorAll('ul');
+    for(let i=0; i<liste.length; i++ ){
+        liste[i].style.backgroundColor='green';
+    }
+}
+
+paintItGreen();
+
 
 // /* ESERCIZIO 9
 //  Scrivi una funzione che rimuova l'ultima lettera dall'h1 ogni volta che l'utente lo clicca
 // */
 
-// const makeItClickable = function () { 
-// }
+const makeItClickable = function () { 
+        titolo.style.cursor='pointer';                  //CAMBIO LO STILE DEL TITOLO QUANDO CI PASSO CON IL MOUSE
+        titolo.addEventListener('click',function() {    //FUNZIONE CHE RISPONDE ALL'IMPULSO OVVERO AL CLICK
+            let scritta=titolo.innerText.split('');     // CON SPLIT DIVIDO LA STRINGA IN UN ARRAY DI SOTTOSTRINGHE
+            console.log(scritta);
+            scritta.pop();                              // CON POP ELIMINO L'ULTIMA OCCORRENZA
+            let nuovoTesto= scritta.join('');           // CON JOIN LE UNISCO
+            console.log(nuovoTesto);
+            titolo.innerText = nuovoTesto;
+        })
+}
+
+makeItClickable();
 
 // /* ESERCIZIO 10
 //  Crea una funzione che, al click sul footer, riveli l'URL del link interno come contenuto di un alert()
 // */
 
-// const revealFooterLink = function () { }
+const revealFooterLink = function () { 
+    const findFooter=document.querySelector('footer');
+    findFooter.style.cursor='pointer';
+    findFooter.addEventListener('click', function() {
+        const footerLink = document.querySelector('footer a');    // VISTO CHE l'URL SI TROVA NELLA <a>         //QUI RICERCA           
+        const linkFooter = footerLink.getAttribute('href');       // DOVENDO RIVELARE, FACCIO getAttribute      //QUI DIAMO ALL'ELEMENTO
+        alert(linkFooter);
+    })
+}
+
+revealFooterLink();
 
 // /* ESERCIZIO 11
 //  Crea una funzione che crei una tabella nell'elemento con id "tableArea". 
 //  La tabella avrà 5 elementi e questa struttura: immagine, nome prodotto, quantità, prezzo
 // */
 
-// const generateTable = function () { }
+// const generateTable = function () { 
+
+
+// }
+
+// generateTable();
 
 // /* ESERCIZIO 12
 //  Crea una funzione che aggiunga una riga alla tabella precedentemente creata e fornisca i dati necessari come parametri
@@ -131,4 +162,9 @@ addParagraph();
 // Crea una funzione che cambi il colore del h2 con id "changeMyColor" con un colore random ad ogni click ricevuto
 // */
 
-// const changeColorWithRandom = function () { }
+// const changeColorWithRandom = function () { 
+//     const newLook = document.get
+
+// }
+
+// changeColorWithRandom();
