cdcdd7548f90320f071243c286b226c5fad8ed57
vitod-ag <116908757+vitod-ag@users.noreply.github.com>
2024-02-05 11:21:00 +0100

    commit rename folder

---
+++ Settimana_3_JAVASCRIPT.2/GIorno.1/lezione/assets/js/script.js
@@ -0,0 +1,8 @@
+
+
+let test = document.getElementById('test');         //SELEZIONE = TRAVERSING
+test.innerHTML = '<h1>DOM</h1>';                    //MANIPOLATION
+
+
+let contenitori = document.getElementsByName('div');
+console.log(contenitori);
